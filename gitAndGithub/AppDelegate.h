c429f8d19a04b0a6c980c77e17ffee8780f4eb75
李宗瑶 <lizongyao>
2016-02-18 17:29:29 +0800

    Initial Commit

---
+++ gitAndGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitAndGithub
+//
+//  Created by 李宗瑶 on 16/2/18.
+//  Copyright © 2016年 先花. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitAndGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitAndGithub
+//
+//  Created by 李宗瑶 on 16/2/18.
+//  Copyright © 2016年 先花. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
